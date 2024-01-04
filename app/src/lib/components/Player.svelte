70db6b2581b3dab5fb1252c6f98e1a86aa6ff2d3
Amezqueta <x.amezqueta@viastore.com>
2024-01-04 10:55:33 +0100

    Reformating

--- app/src/lib/components/Player.svelte
+++ app/src/lib/components/Player.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-  import { T } from '@threlte/core';
-  import {BoxGeometry, Color, MeshStandardMaterial, Vector3} from 'three';
-  import {onDestroy} from "svelte";
+    import {T} from '@threlte/core';
+    import {BoxGeometry, Color, MeshStandardMaterial, Vector3} from 'three';
+    import {onDestroy} from "svelte";
 
-  const material = new MeshStandardMaterial({ color: new Color(0xff3f00) });
-  const geometry = new BoxGeometry(2, 2, 2);
+    const material = new MeshStandardMaterial({color: new Color(0xff3f00)});
+    const geometry = new BoxGeometry(2, 2, 2);
 
-  export let position = [0, 0, 0];
+    export let position = [0, 0, 0];
 
-  export function Move(vector: Vector3) {
-      position[0] += vector.x;
-      position[1] += vector.y;
-      position[2] += vector.z;
-  }
-  
-  onDestroy(() => {
-      console.log("Player destroyed");
-  })
-  
+    export function Move(vector: Vector3) {
+        position[0] += vector.x;
+        position[1] += vector.y;
+        position[2] += vector.z;
+    }
+
+    onDestroy(() => {
+        console.log("Player destroyed");
+    })
+
 </script>
 
-<T.Mesh castShadow {geometry} {material} position={position} />
+<T.Mesh castShadow {geometry} {material} position={position}/>
--- app/src/lib/components/Scene.svelte
+++ app/src/lib/components/Scene.svelte
@@ -1,94 +1,93 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
-  import { onMount } from 'svelte';
-  import Player from '$lib/components/Player.svelte';
-  import {ContactShadows, Grid, OrbitControls} from "@threlte/extras";
-  import {Vector3} from "three";
+    import {T} from '@threlte/core'
+    import {onMount} from 'svelte';
+    import Player from '$lib/components/Player.svelte';
+    import {ContactShadows, Grid, OrbitControls} from "@threlte/extras";
+    import {Vector3} from "three";
 
-  export let socket: any;
-  let player : any = null;
-  let socketConnected = false;
+    export let socket: any;
+    let player: any = null;
+    let socketConnected = false;
 
-  onMount(() => {
-    window.addEventListener('keydown', handleKeyDown);
+    onMount(() => {
+        window.addEventListener('keydown', handleKeyDown);
 
-    socket.on('connect', () => {
-      socketConnected = true;
+        socket.on('connect', () => {
+            socketConnected = true;
+        });
+
+        socket.on('disconnect', () => {
+            socketConnected = false;
+            if (player) {
+                player.$destroy();
+                player = null;
+            }
+        })
+
     });
-    
-    socket.on('disconnect', () => {
-      socketConnected = false;
-      if(player)
-      {
-        player.$destroy();
-        player = null;
-      }
-    })
-   
-  });
-  
-  $: console.log("Socket connected: "+socketConnected);
 
-  $: if (socketConnected && !player) {
-      player = new Player({
-        target: document.body
-      });
-  }
+    $: console.log("Socket connected: " + socketConnected);
 
-  function handleKeyDown(event: any) {
-    console.log(event.key);
-    
-    switch (event.key){
-      case "w":
-        player.Move(new Vector3(1, 0, 0));
-        break;
-      case "a":
-        player.Move(new Vector3(0, 0, -1));
-        break;
-      case "s":
-        player.Move(new Vector3(-1, 0, 0));
-        break;
-      case "d":
-        player.Move(new Vector3(0, 0, 1));
-        break;
+    $: if (socketConnected && !player) {
+        player = new Player({
+            target: document.body
+        });
     }
-    if (socketConnected) {
-      socket.emit('mensaje', event.key);
+
+    function handleKeyDown(event: any) {
+        console.log(event.key);
+
+        switch (event.key) {
+            case "w":
+                player.Move(new Vector3(1, 0, 0));
+                break;
+            case "a":
+                player.Move(new Vector3(0, 0, -1));
+                break;
+            case "s":
+                player.Move(new Vector3(-1, 0, 0));
+                break;
+            case "d":
+                player.Move(new Vector3(0, 0, 1));
+                break;
+        }
+        if (socketConnected) {
+            socket.emit('mensaje', event.key);
+        }
     }
-  }
 </script>
 
 <T.PerspectiveCamera
-  makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
+        makeDefault
+        position={[-10, 10, 10]}
+        fov={15}
 >
-  <OrbitControls
-    enableZoom={true}
-    enableDamping
-    target.y={1.5}
-  />
+    <OrbitControls
+            enableZoom={true}
+            enableDamping
+            target.y={1.5}
+    />
 </T.PerspectiveCamera>
 
 <T.DirectionalLight
-  intensity={1}
-  position.x={5}
-  position.y={10}
+        intensity={1}
+        position.x={5}
+        position.y={10}
 />
-<T.AmbientLight intensity={0.5} />
+<T.AmbientLight intensity={0.5}/>
 
 <Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
+        position.y={-0.001}
+        cellColor="#ffffff"
+        sectionColor="#ffffff"
+        sectionThickness={0}
+        fadeDistance={25}
+        cellSize={2}
 />
 
 <ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
+        scale={10}
+        blur={2}
+        far={2.5}
+        opacity={0.5}
 />
