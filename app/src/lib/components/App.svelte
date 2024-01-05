b6e12ab0618506c4f64d77ac22c90a2c413fd91e
amezqueta <amezqueta2@gmail.com>
2024-01-05 21:18:00 +0100

    the scene is wrapped with World for rapier Waits for the creation of the Scene before trying to connect to it

--- app/src/lib/components/App.svelte
+++ app/src/lib/components/App.svelte
@@ -4,43 +4,63 @@
     import UI from './UI.svelte'
     import io from 'socket.io-client'
     import {onMount} from 'svelte';
+    import { World } from '@threlte/rapier';
 
     let socket: any = null;
 
-
     let scene: any;
+
+    const waitForScene = () => new Promise<void>((resolve) => {
+        const checkScene = () => {
+            if (scene) {
+                resolve();
+            } else {
+                setTimeout(checkScene, 50);
+            }
+        };
+        checkScene();
+    });
+
 
     onMount(() => {
 
-        socket = io('http://localhost:3000'); // Conectar al servidor desde el cliente
+        socket = io('http://localhost:3000');
 
         socket.on('user-connected', (userId: string) => {
-            scene?.OnUserConnected(userId);
+            waitForScene().then(() => {
+                scene.OnUserConnected(userId);
+            });
         });
 
         socket.on('user-disconnected', (userId: string) => {
-            scene?.OnUserDisconnected(userId)
-        })
-
+            waitForScene().then(() => {
+                scene.OnUserDisconnected(userId);
+            });
+        });
+        
         socket.on('connected', (playerData: any, serverPlayers: any[]) => {
-            scene?.OnConnected(playerData, serverPlayers);
+            waitForScene().then(() => {
+                scene.OnConnected(playerData, serverPlayers);
+            });
         });
 
         socket.on('disconnect', () => {
-            scene?.OnDisconnected();
+            scene.OnDisconnected();
         })
 
         socket.on('msg', (message: string) => {
             console.log(message);
         })
     });
+    
 </script>
 
 
 <div style="position:relative; height:100%; width:100%; background-color: rgb(14,22,37)">
     <Canvas>
         <UI socket={socket}/>
-        <Scene bind:this={scene} socket={socket}/>
+        <World>
+            <Scene bind:this={scene} socket={socket}/>
+        </World>
     </Canvas>
 </div>
-    
