52bbdbc8d884728b926e4ca422e15bd5c271d624
Amezqueta <x.amezqueta@viastore.com>
2024-01-15 13:27:50 +0100

    The onlineplayer has a rigidbody

--- app/src/lib/components/Players/OnlinePlayer.svelte
+++ app/src/lib/components/Players/OnlinePlayer.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+    import { type Collider as RCollider, type RigidBody as RRigidBody } from "@dimforge/rapier3d-compat";
     import { onMount } from "svelte";
     import { T } from "@threlte/core";
     import { BoxGeometry, Group, MeshStandardMaterial } from "three";
     import { onDestroy } from "svelte";
     import TextBillboard from "../TextBillboard.svelte";
-    import { Collider } from "@threlte/rapier";
+    import { Collider, RigidBody } from "@threlte/rapier";
 
     const material = new MeshStandardMaterial();
     const geometry = new BoxGeometry(2, 2, 2);
@@ -17,6 +18,7 @@
 
     export function SetPosition(newPos: any) {
         position = [newPos.x, newPos.y, newPos.z];
+        rigidBody.setTranslation(newPos, true);
     }
 
     export function SetRotation(newRot: number) {
@@ -42,9 +44,11 @@
     $: {
         material.color.set(color);
     }
+    let rigidBody: RRigidBody;
 </script>
 
-<T.Group {position}>
+<RigidBody bind:rigidBody type="kinematicPosition" enabledRotations={[false, false, false]}>
     <T.Mesh castShadow {geometry} {material} rotation.y={meshRotation} />
     <TextBillboard text={nick} position={[0, 3, 0]} {color} />
-</T.Group>
+    <Collider shape="capsule" args={[0.3, 1]} />
+</RigidBody>
